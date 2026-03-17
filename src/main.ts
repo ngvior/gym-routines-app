1b7b1d1246d54b3616860b9a57b23a278e02a21b
ngvior <ngvior@gmail.com>
2026-03-17 18:02:35 -0300

    1.1.3: installed @nestjs/config and updated env variables use in app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  const port = process.env.PORT ?? 8000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
